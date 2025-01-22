f7ca4230a5fe2d98e3988b7ec9bad0a6a6c6e729
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-01-22 14:31:54 +0530

    Add delete user

--- app/src/main/java/com/example/swiftmart/Frgments/AccountFragment.java
+++ app/src/main/java/com/example/swiftmart/Frgments/AccountFragment.java
@@ -1,13 +1,18 @@
 package com.example.swiftmart.Frgments;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,13 +27,27 @@ import com.example.swiftmart.Account.Language_Activity;
 import com.example.swiftmart.Account.OrdersActivity;
 import com.example.swiftmart.Account.WishlistActivity;
 import com.example.swiftmart.LoginActivity;
+import com.example.swiftmart.MainActivity;
 import com.example.swiftmart.R;
+import com.example.swiftmart.WelcomeActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class AccountFragment extends Fragment {
@@ -40,6 +59,7 @@ public class AccountFragment extends Fragment {
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private ProgressBar accountFragmentProgressBar;
+    private AlertDialog dialog;
 
     public AccountFragment() {
 
@@ -106,12 +126,9 @@ public class AccountFragment extends Fragment {
         uid = mAuth.getCurrentUser().getUid();
         DocumentReference reference = db.collection("Users").document(uid);
 
-        reference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (value != null && value.exists()){
-                    accountFragmentUserName.append(value.getString("Username").split(" ")[0]);
-                }
+        reference.addSnapshotListener((value, error) -> {
+            if (value != null && value.exists()){
+                accountFragmentUserName.append(value.getString("Username").split(" ")[0]);
             }
         });
 
@@ -119,103 +136,76 @@ public class AccountFragment extends Fragment {
 
     // handle edit profile click
     private void handleEditProfileClick(){
-        btnEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), Edit_profile_Activity.class);
-                startActivity(intent);
-            }
+        btnEditProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), Edit_profile_Activity.class);
+            startActivity(intent);
         });
 
-        llEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), Edit_profile_Activity.class);
-                startActivity(intent);
-            }
+        llEditProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), Edit_profile_Activity.class);
+            startActivity(intent);
         });
     }
 
     // handle order history click
     private void handleOrderHistoryClick(){
-        btnOrderHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), OrdersActivity.class);
-                startActivity(intent);
-            }
+        btnOrderHistory.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), OrdersActivity.class);
+            startActivity(intent);
         });
 
-        llOrderHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), OrdersActivity.class);
-                startActivity(intent);
-            }
+        llOrderHistory.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), OrdersActivity.class);
+            startActivity(intent);
         });
     }
 
     // handle select language click
     private void handleSelectLanguageClick(){
-        btnLanguage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), Language_Activity.class);
-                startActivity(intent);
-            }
+        btnLanguage.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), Language_Activity.class);
+            startActivity(intent);
         });
 
-        llLanguage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), Language_Activity.class);
-                startActivity(intent);
-            }
+        llLanguage.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), Language_Activity.class);
+            startActivity(intent);
         });
     }
 
     // handle wishlist click
     private void handleWishlistClick(){
-        btnWishlist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), WishlistActivity.class);
-                startActivity(intent);
-            }
+        btnWishlist.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), WishlistActivity.class);
+            startActivity(intent);
         });
 
-        llWishlist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), WishlistActivity.class);
-                startActivity(intent);
-            }
+        llWishlist.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), WishlistActivity.class);
+            startActivity(intent);
         });
     }
 
     // handle saved address click
     private void handleSavedAddressClick(){
-        llsavedaddress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), Address_Activity.class);
-                startActivity(intent);
-            }
+        llsavedaddress.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), Address_Activity.class);
+            startActivity(intent);
         });
 
-        btnAddress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), Address_Activity.class);
-                startActivity(intent);
-            }
+        btnAddress.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), Address_Activity.class);
+            startActivity(intent);
         });
     }
 
-    // TODO handle delete account click
+    // handle delete account click
     private void handleDeleteAccountClick(){
+        btnDeleteUser.setOnClickListener(v -> displayDialog());
 
+        llDeleteUser.setOnClickListener(v -> displayDialog());
     }
+
 
     // TODO handle about us click
     private void handleAboutUsClick(){
@@ -230,6 +220,43 @@ public class AccountFragment extends Fragment {
     // TODO handle rate us click
     private void handleRateUsClick(){
 
+    }
+
+    private void displayDialog(){
+        dialog = new AlertDialog.Builder(getContext())
+                .setView(R.layout.delete_account_dialog)
+                .setCancelable(false)
+                .create();
+        dialog.show();
+
+        AppCompatButton deleteButton = dialog.findViewById(R.id.deleteButton);
+        AppCompatButton cancelButton = dialog.findViewById(R.id.cancelButton);
+
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Users")
+                        .document(uid)
+                        .delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                mAuth.signOut();
+                                if (getContext() != null) {
+                                    Intent intent = new Intent(getContext(), WelcomeActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+
+                dialog.dismiss();
+            }
+        });
+
+        cancelButton.setOnClickListener(v -> dialog.dismiss());
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 
     // handle onBack press
@@ -249,16 +276,13 @@ public class AccountFragment extends Fragment {
 
     // handle user logout
     private void handleUserLogout(){
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuth.signOut();
-                if (getContext() != null){
-                    Intent intent = new Intent(getContext(), LoginActivity.class);
-                    startActivity(intent);
-                    progress();
-                    getActivity().finish();
-                }
+        btnLogout.setOnClickListener(v -> {
+            mAuth.signOut();
+            if (getContext() != null){
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                progress();
+                getActivity().finish();
             }
         });
     }
