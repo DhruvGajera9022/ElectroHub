d6100acd5f259afa9bfae588ffce51c4039366ff
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-02-26 12:17:42 +0530

    Remove login first

--- app/src/main/java/com/example/swiftmart/WelcomeActivity.java
+++ app/src/main/java/com/example/swiftmart/WelcomeActivity.java
@@ -63,10 +63,10 @@ public class WelcomeActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
 
-        if (mAuth.getCurrentUser() != null){
-            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-            finish();
-        }
+//        if (mAuth.getCurrentUser() != null){
+//            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+//            finish();
+//        }
 
         welcomeScrollView.setVerticalScrollBarEnabled(false);
     }
